13ccfb10a8ca9915149a342f5f641ddf85586090
Opilane TTHK <johannat2009@gmail.com>
2025-11-28 11:23:57 +0200

    Arvutamine (MaaKalkulaator)

---
+++ MaaKalkulaator/MaaKalkulaator/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MaaKalkulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Arvutada välja Maa ümbermõõt ja mitu münti
+            //Mahub järjestikku ümber Maa
+            //Arvutades tohib kasutada ainult mündi diameetrit ja maa raadiust
+
+            //Mündi diameeter: 25.75 mm
+            //Maa raadius: 6 371 km
+
+            double CoinDiameter = 25.75; //mm
+            double earthradius = 637100000;
+            double aroundEarth = 2 * Math.PI * earthradius;
+
+            Console.WriteLine(aroundEarth);
+            double howManyCoins = aroundEarth / CoinDiameter;
+
+            Console.WriteLine("kahe eurosid läheb ümber maa {0:0}", howManyCoins);
+        }
+    }
+}
