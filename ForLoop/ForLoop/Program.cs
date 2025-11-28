fba6f807668c84e97629f54eb33de53b81a7b185
Opilane TTHK <johannat2009@gmail.com>
2025-11-28 09:54:00 +0200

    wHILENesting

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Loop");
+
+            int loop = 10;
+
+            //int i muutuja määratlemine ja selle väärtus on 0
+            //i < loop igakord võrdleb loop muutuja, et kas on suurem i-st
+            //i++ Liidab iag kord ühe korra juurde e. sama hea, kui i + 1
+            for (int i = 0; i < loop; i++)
+            {
+                Console.WriteLine("Loop " + i);
+                Thread.Sleep(1000);
+                Console.Beep();
+            }
+        }
+    }
+}
