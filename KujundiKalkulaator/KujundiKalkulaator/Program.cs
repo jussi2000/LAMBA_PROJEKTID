e28f105043d56e9dae17daf5fa56d1a9dbf5212c
Opilane TTHK <johannat2009@gmail.com>
2025-11-28 10:04:03 +0200

    WhileNestinggggggg

---
+++ KujundiKalkulaator/KujundiKalkulaator/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace KujundiKalkulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Ruudu sees asub ring, Ringi raadius on 3 ühikut.
+            //Leia ja väljasta ekraanile ruudu pindala ja ümbermõõt
+        }
+    }
+}
