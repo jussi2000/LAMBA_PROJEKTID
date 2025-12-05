6d969c712df948ff047769c2a9fc8b419a1dacaf
Opilane TTHK <johannat2009@gmail.com>
2025-12-05 11:28:54 +0200

    ForKillKoll

---
+++ ForKillKoll/ForKillKoll/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ForKillKoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Eelnevaid teadmisi kasutades kirjuta programm, mis väljastaks:
+            //kill - koll
+            //kill - koll
+            //killadi - koll
+            //kill - koll
+            //kill - koll
+            //killadi - koll
+            //kill - koll
+            //kill - koll
+            //killadi - koll
+            //kill - koll
+
+            Console.WriteLine("Sisesta korduste arv");
+            int arv = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < arv; i++)
+            {
+
+                //nüüd siia lisada kaks loopi
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine("Kill-Koll");
+                }
+                for (int j = 0; j < 1; j++)
+                {
+                    Console.WriteLine("Killadi-Koll");
+                }
+                {
+                    Console.WriteLine("Kill-Koll");
+                }
+            }
+        }
+    }
+}
