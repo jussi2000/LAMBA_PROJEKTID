9eeeef58e5466e5d39ba082ebc1ddbe17db64aec
Opilane TTHK <johannat2009@gmail.com>
2025-12-05 10:36:39 +0200

    ForAsterisk

---
+++ ForAsterisk/ForAsterisk/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ForAsterisk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Asterisk!");
+
+            int nr = Convert.ToInt32(Console.ReadLine());
+
+            //tuleb kasutada for loopi
+            //sisesta ridade arvu ja tuleb vastav ridade arv konsooli
+            //peab näitama tärne
+            for (int i = 0; i < nr ; i++ )
+            {
+                Console.WriteLine("*");
+            }
+        }
+    }
+}
