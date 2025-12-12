ce7cc369339018c41cb54f9686ff7164119bf647
Opilane TTHK <johannat2009@gmail.com>
2025-12-12 11:08:51 +0200

    ForLoopTriangle

---
+++ ForLoopTriangle/ForLoopTriangle/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ForLoopTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tärnidest kolmnurk e poolik püramiid");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("Sisesta ridad arv: ");
+            int i, j, rows;
+            rows = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("*", j);
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
