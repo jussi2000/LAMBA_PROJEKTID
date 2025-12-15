22821eb2a016326e0a38e15ffd77b4c89250b7e8
Opilane TTHK <johannat2009@gmail.com>
2025-12-15 09:58:25 +0200

    FibonacciFor

---
+++ FibonacciFor/FibonacciFor/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FibonacciFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n\n");
+            Console.WriteLine("Näita esimest Fibonacci seeriat");
+            Console.WriteLine("-------------------------------\n\n");
+            Console.WriteLine("Sisesta number");
+            int prv = 0, pre = 1, trm, i, n;
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Siin on Fibronacci seeria {0} kuni \n", n);
+
+            Console.Write($"{0} {1}", prv, pre);
+
+            for (i = 3; i <= n; i++)
+            {
+                trm = prv + pre;
+                Console.Write(trm + " ");
+                prv = pre;
+                pre = trm;
+            }
+            Console.Write("\n");
+        }   
+    }
+}
