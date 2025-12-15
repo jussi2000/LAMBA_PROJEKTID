ac1634639564e2c9ab621ffecd9e2d85eb28a231
Opilane TTHK <johannat2009@gmail.com>
2025-12-15 09:05:40 +0200

    DecimalToBinary

---
+++ DecimalToBinary/DecimalToBinary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+namespace DecimalToBinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decimnal to binary");
+
+            Console.WriteLine("Sisesta number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string binaryNumber = "";
+
+            while (num > 0)
+            {
+                int reminder = num % 2;
+                binaryNumber = reminder + binaryNumber;
+                num /= 2;
+            }
+            Console.WriteLine(binaryNumber);
+
+        }
+    }
+}
