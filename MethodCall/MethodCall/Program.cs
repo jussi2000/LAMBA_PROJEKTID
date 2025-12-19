7d8c965ef4f9d973f356b48f4da2ac53e66e668d
Opilane TTHK <johannat2009@gmail.com>
2025-12-19 11:00:49 +0200

    MethodCall

---
+++ MethodCall/MethodCall/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MethodCall
+{
+    internal class Program
+    {
+        //Kutsuda meetod välja
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            SayHello();
+            SayGoodbye();
+        }
+        static void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+        static void SayGoodbye()
+        {
+            Console.WriteLine("Goodbye");
+
+        }
+    }
+}
