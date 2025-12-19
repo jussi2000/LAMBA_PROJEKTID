5d5cc370b164098a0a073dc73886d8543115e8b9
Opilane TTHK <johannat2009@gmail.com>
2025-12-19 11:44:29 +0200

    RectangleFor

---
+++ RectangleFor/RectangleFor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+
+namespace RectangleFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta ristküliku kshr küljr mõõdud");
+            Console.WriteLine("Pikkus");
+            double lenght = double.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine("Laius");
+            double Width = double.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= lenght; row++)
+            {
+                for (int column = 1; column <= Width; column++)
+                {
+                    string mark;
+                    if (row == column || row + column <= +1)
+                    {
+                        mark = "*";
+                    }
+                    else
+                    {
+                        mark = "*";
+                    }
+                    Console.Write(mark);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
