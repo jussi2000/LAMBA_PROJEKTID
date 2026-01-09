99839d5e24ca5bc556b754ad88c9365ffc7acaf6
Opilane TTHK <johannat2009@gmail.com>
2026-01-09 11:46:26 +0200

    Rectangle

---
+++ Rectangle/Rectangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+
+namespace Rectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta ristküliku suurus");
+            //teha ristkülik tärnidest
+            //peab arvutama pindala ja ümbermõõdu
+            Console.WriteLine("Sisesta pikkus");
+            int row = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sisesta laius");
+            int col = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            int area = row * col;
+            int around = 2 * (row * col);
+            Console.WriteLine("Pindala on " + area);
+            Console.WriteLine("Ümbermõõt on " + around);
+        }
+    }
+}
