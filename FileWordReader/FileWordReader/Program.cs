27574bb7948b6950ddaff0ec6b2f81ed4e68abf1
Opilane TTHK <johannat2009@gmail.com>
2026-01-16 11:43:40 +0200

    FileWordReader

---
+++ FileWordReader/FileWordReader/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace FileWordReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Luuletus");
+
+            FileWordReader();
+        }
+
+        static void FileWordReader()
+        {
+            string line;
+
+            try
+            {
+                using(StreamReader sr = new StreamReader("C:/Users/opilane/Desktop/luuletus.txt"))
+                {
+                    int cnt = 0; //rea muutuja
+                    while (sr.EndOfStream == false)
+                    {
+                        cnt++; //rea nr ühe ühiku võrra
+                        line = sr.ReadLine(); //teksti lugemine txt failist
+                        int pikkus = line.Length; //sõna lugemine, kui sõne pikkusena
+                        Console.WriteLine(cnt + " " + line + " " + pikkus);
+                    }
+                    sr.Close(); //faili lugemise lõpetamine
+                }
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine("Faili ei saa lugeda"); 
+                Console.WriteLine(e.Message);
+                throw;
+
+            }
+        }
+    }
+}
